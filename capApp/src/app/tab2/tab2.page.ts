ce1e28d22c2e0ec488f077864e92afa9277dc223
nathancread <nathancread@gmail.com>
2019-10-31 10:44:20 -0500

    initial commit with a boilerplate app

---
+++ capApp/src/app/tab2/tab2.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+  //constructor() {}
+  photo: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {  }
+
+
+  async takePicture(){
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
+  }
+
+}
