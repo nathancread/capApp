75a7d6423d1bd02f3ce443f36587f5b98131918e
nathancread <nathancread@gmail.com>
2019-11-26 00:48:05 -0600

    added start and stop for compass

--- capApp/src/app/tab4/tab4.page.ts
+++ capApp/src/app/tab4/tab4.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
 import { DeviceOrientation, DeviceOrientationCompassHeading } from '@ionic-native/device-orientation/ngx';
+import { Subscription } from 'rxjs';
+import { Events } from '@ionic/angular';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-tab4',
@@ -11,12 +14,16 @@ export class Tab4Page {
   magneticReading: any;
   trueReading: any;
   compassReading: DeviceOrientationCompassHeading;
-
+  btnText: any
+  subsciptionCompass : any;
 
 
   constructor(
     private deviceOrientation: DeviceOrientation
-  ) {}
+  ) 
+  {
+    this.btnText = "Read Compass";
+  }
 
   getCompass()
   {
@@ -28,9 +35,30 @@ export class Tab4Page {
   this.magneticReading = this.compassReading.magneticHeading;
   this.trueReading = this.compassReading.trueHeading;
 
-  
   }
     
-  
-  
+  startCompass()
+  {
+    this.subsciptionCompass = this.deviceOrientation.watchHeading().subscribe(
+      (data: DeviceOrientationCompassHeading) => this.magneticReading = data.magneticHeading
+    );
+
+    //this.magneticReading = this.compassReading.magneticHeading;
+    //this.trueReading = this.compassReading.trueHeading;
+  }
+  endCompass()
+  {
+    this.subsciptionCompass.unsubscribe();
+  }
+
+  check() {
+    if (this.btnText == 'Read Compass') {
+       this.startCompass();
+       this.btnText = 'End Compass';
+    } else if(this.btnText == 'End Compass') {
+       this.endCompass();
+       this.btnText = 'Read Compass';
+    }
+ }
+
 }
