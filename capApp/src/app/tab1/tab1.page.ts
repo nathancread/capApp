67aae2cdf45bca8e0a2314ad066cef3ebec28ec9
nathancread <nathancread@gmail.com>
2019-12-13 15:31:43 -0600

    picture and geo working

--- capApp/src/app/tab1/tab1.page.ts
+++ capApp/src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+import { DeviceOrientation, DeviceOrientationCompassHeading } from '@ionic-native/device-orientation/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -8,8 +11,55 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 export class Tab1Page {
   
+  //check marks
+  compassButton: string;
+  pictureButton: string;
+
+
+  //holds camera output
   currentImage: any;
-  constructor(private camera: Camera) {}
+
+  //holds geoloc output
+  geoLatitude: number;
+  geoLongitude: number;
+  geoAccuracy:number;
+  geoAddress: string;
+  watchLocationUpdates:any; 
+  loading:any;
+  isWatching:boolean;
+
+    //Geocoder configuration
+    geoencoderOptions: NativeGeocoderOptions = {
+      useLocale: true,
+      maxResults: 5
+    };
+
+    //compass vars
+    magneticReading: any;
+    trueReading: any;
+    compassReading: DeviceOrientationCompassHeading;
+    btnText: any
+    subsciptionCompass : any;
+
+  constructor(
+    private geolocation: Geolocation,
+    private nativeGeocoder: NativeGeocoder,
+    private deviceOrientation: DeviceOrientation,
+    private camera: Camera) 
+    {
+      this.btnText = "Read Compass";
+      this.compassButton ="radio-button-off";
+      this.pictureButton ="radio-button-off";
+
+    }
+
+
+
+  ////////////
+  //Camera code
+  /////////////
+
+
 
   takePicture() {
     const options: CameraOptions = {
@@ -21,10 +71,101 @@ export class Tab1Page {
 
     this.camera.getPicture(options).then((imageData) => {
       this.currentImage = 'data:image/jpeg;base64,' + imageData;
+      this.pictureButton = "checkmark-circle-outline";
     }, (err) => {
       // Handle error
       console.log("Camera issue:" + err);
     });
   }
+
+
+
+  /////////////
+  ///geolocator code
+  ////////////
+
+
+  //Get current coordinates of device
+  getGeolocation(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.geoLatitude = resp.coords.latitude;
+      this.geoLongitude = resp.coords.longitude; 
+      this.geoAccuracy = resp.coords.accuracy; 
+      this.getGeoencoder(this.geoLatitude,this.geoLongitude);
+     }).catch((error) => {
+       alert('Error getting location'+ JSON.stringify(error));
+     });
+  }
+
+  //geocoder method to fetch address from coordinates passed as arguments
+  getGeoencoder(latitude,longitude){
+    this.nativeGeocoder.reverseGeocode(latitude, longitude, this.geoencoderOptions)
+    .then((result: NativeGeocoderResult[]) => {
+      this.geoAddress = this.generateAddress(result[0]);
+    })
+    .catch((error: any) => {
+      alert('Error getting location'+ JSON.stringify(error));
+    });
+  }
+
+  //Return Comma saperated address
+  generateAddress(addressObj){
+      let obj = [];
+      let address = "";
+      for (let key in addressObj) {
+        obj.push(addressObj[key]);
+      }
+      obj.reverse();
+      for (let val in obj) {
+        if(obj[val].length)
+        address += obj[val]+', ';
+      }
+    return address.slice(0, -2);
+  }
+
+
+
+//////////////////
+/// COMPASS SUTFF/////
+/////////////////
+
+
+
+  getCompass()
+  {
+  // Get the device current compass heading
+  this.deviceOrientation.getCurrentHeading().then(
+    (data: DeviceOrientationCompassHeading) => this.compassReading = data,
+    (error: any) => console.log(error)
+  );
+  this.magneticReading = this.compassReading.magneticHeading;
+  this.trueReading = this.compassReading.trueHeading;
+
+  }
+    
+  startCompass()
+  {
+    this.subsciptionCompass = this.deviceOrientation.watchHeading().subscribe(
+      (data: DeviceOrientationCompassHeading) => this.magneticReading = data.magneticHeading
+    );
+
+    //this.magneticReading = this.compassReading.magneticHeading;
+    //this.trueReading = this.compassReading.trueHeading;
+  }
+  endCompass()
+  {
+    this.subsciptionCompass.unsubscribe();
+  }
+
+  check() {
+    if (this.btnText == 'Read Compass') {
+       this.startCompass();
+       this.btnText = 'End Compass';
+    } else if(this.btnText == 'End Compass') {
+       this.endCompass();
+       this.compassButton = "checkmark-circle-outline";
+       this.btnText = 'Read Compass';
+    }
+ }
 
 }
